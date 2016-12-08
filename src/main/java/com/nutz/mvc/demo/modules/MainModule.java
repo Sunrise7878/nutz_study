6230ab5c825a429c44f8df0dc530c13c5b2bde8a
gancy <18451117878@163.com>
2016-12-08 18:10:44 +0800

    ioc

--- src/main/java/com/nutz/mvc/demo/modules/MainModule.java
+++ src/main/java/com/nutz/mvc/demo/modules/MainModule.java
@@ -1,11 +1,16 @@
 package com.nutz.mvc.demo.modules;
 
+import org.nutz.ioc.Ioc;
+import org.nutz.ioc.impl.NutIoc;
+import org.nutz.ioc.loader.combo.ComboIocLoader;
 import org.nutz.mvc.annotation.At;
 import org.nutz.mvc.annotation.Fail;
 import org.nutz.mvc.annotation.IocBy;
 import org.nutz.mvc.annotation.Modules;
 import org.nutz.mvc.annotation.Ok;
 import org.nutz.mvc.ioc.provider.ComboIocProvider;
+
+import com.nutz.mvc.demo.entity.User;
 
 @Modules(scanPackage = true)
 @IocBy(type = ComboIocProvider.class , args = {"*js" , "conf/ioc/" , "com.nutz.mvc.demo"})
@@ -40,6 +45,13 @@ public class MainModule {
 	@Fail("void")
 	public void voidView(){
 		
+	}
+	
+	//ioc试验
+	public static void main(String[] args) throws ClassNotFoundException {
+		Ioc ioc = new NutIoc(new ComboIocLoader("conf/ioc/iocbean.js"));
+		User user = ioc.get(User.class,"xiaogan");
+		System.out.println("name:" + user.getUserName() + "   passwd:" + user.getPasswd() + "   age:" + user.getAge());
 	}
 	
 	
---
+++ src/main/resources/conf/ioc/iocbean.js
@@ -0,0 +1,8 @@
+var ioc={
+	xiaogan:{
+		type:'com.nutz.mvc.demo.entity.User',
+		userName : '小甘',
+		passwd : '123456',
+		age : '18'
+	}
+}
