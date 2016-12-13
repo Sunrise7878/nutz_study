c179a87f923a06142793cdf5c9b49f74010b5ec3
gancy <18451117878@163.com>
2016-12-13 16:40:53 +0800

    新增注册测试

--- src/test/java/com/nutz/mvc/demo/test/TestUserModule.java
+++ src/test/java/com/nutz/mvc/demo/test/TestUserModule.java
@@ -7,7 +7,6 @@ import org.nutz.log.Log;
 import org.nutz.log.Logs;
 
 import com.nutz.mvc.demo.modules.UserModule;
-import com.nutz.mvc.demo.utils.Result;
 
 public class TestUserModule extends TestBase{
 	
@@ -23,24 +22,18 @@ public class TestUserModule extends TestBase{
 		scanner = new Scanner(System.in);
 	}
 	
-	/*@Test
-	public void TestLogin() throws Exception{
-		System.out.print("输入账号:");
-		String userName = scanner.nextLine();
-		System.out.print("输入密码:");
-		String passwd = scanner.nextLine();
-		Result result = userMoudle.login(userName, passwd, null);
-		log.debug("请求结果:" + result.getStatus());
-		log.debug("请求消息:" + result.getMessage());
-		log.debug("请求数据:" + result.getData());
-	}*/
-	
+	//注册测试
 	@Test
-	public void TestRegister() throws Exception{
-		
+	public void register() throws Exception{
+		System.out.print("请输入账号:");
+		String userName = scanner.nextLine();
+		System.out.print("请输入密码:");
+		String passwd = scanner.nextLine();
+		String result = userMoudle.register(userName, passwd, null, null);
+		System.out.println("");
+		log.debug("访问结果: " + result);
 	}
 	
 	@Override
-	protected void _after() throws Exception {
-	}
+	protected void _after() throws Exception {}
 }
